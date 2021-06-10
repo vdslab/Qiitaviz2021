82fca4a0b0dbcdb4af764483d169ec7244d2eaef
上月那奈子 <chna18018@g.nihon-u.ac.jp>
2021-06-10 22:57:13 +0900

    commit2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 function App() {
   return (
     <div>
-      <h1>O S A K E !</h1>
-      <h1>N O M E N A I !</h1>
+      <section class="hero is-link is-medium">
+        <div class="hero-head">
+          <nav class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <a class="navbar-item">
+                  <img
+                    src="https://bulma.io/images/bulma-type-white.png"
+                    alt="Logo"
+                  />
+                </a>
+                <span class="navbar-burger" data-target="navbarMenuHeroA">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              <div id="navbarMenuHeroA" class="navbar-menu">
+                <div class="navbar-end">
+                  <a class="navbar-item is-active">Home</a>
+                  <a class="navbar-item">Examples</a>
+                  <a class="navbar-item">Documentation</a>
+                  <span class="navbar-item">
+                    <a class="button is-primary is-inverted">
+                      <span class="icon">
+                        <i class="fab fa-github"></i>
+                      </span>
+                      <span>Download</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">2020 ©CASAREAL</div>
+          </div>
+        </footer>
+      </section>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { render } from "react-dom";
+import "bulma/css/bulma.css";
 import App from "./App";
 
 render(<App />, document.querySelector("#content"));
