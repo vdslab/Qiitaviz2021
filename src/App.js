fd1cf6d51da54a5bf3821efb5fea21e52af6b924
unibon4141 <vhcdyufg@gmail.com>
2021-06-17 19:02:06 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import D3DirectedGraph from "./D3DirectedGraph";
+function App() {
+  return (
+    <div class="container">
+      <h1>DirectedGraph</h1>
+      <D3DirectedGraph />
+    </div>
+  )
+}
+export default App;
