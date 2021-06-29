687acb1947d33eda0d28980f93369781ac0485e0
unibon4141 <vhcdyufg@gmail.com>
2021-06-30 00:21:38 +0900

    モバイル対応

--- src/App.js
+++ src/App.js
@@ -1,10 +1,146 @@
 import D3DirectedGraph from "./D3DirectedGraph";
+import "bulma/css/bulma.css";
 function App() {
   return (
-    <div class="container">
-      <h1>DirectedGraph</h1>
-      <D3DirectedGraph />
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
-  )
+  );
+}
+function Main() {
+  return (
+    <section className="section has-background-white-bis">
+      <div className="container">
+        <div className="columns is-multiline is-centered">
+          <AreaTab />
+          <Search />
+        </div>
+
+        <D3DirectedGraph />
+      </div>
+    </section>
+  );
+}
+function Header() {
+  return (
+    <div className="hero is-small is-primary">
+      <section className="section hero-body">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third is-offset-one-third">
+              <h1 className="title is-3 has-text-centered">
+                2021QiitaViz-legends
+              </h1>
+            </div>
+            <Navigation />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+function AreaTab() {
+  return (
+    <div className="column is-one-fifth">
+      <div className="dropdown is-hoverable">
+        <div className="dropdown-trigger">
+          <button
+            className="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+          >
+            <span>領域を選択</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down"></i>
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            <a href="#" className="dropdown-item">
+              Web
+            </a>
+            <a href="#" className="dropdown-item">
+              IOS
+            </a>
+            <a href="#" className="dropdown-item">
+              Android
+            </a>
+            <a href="#" className="dropdown-item">
+              CS
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+function Search() {
+  return (
+    <div className="column is-one-fifth">
+      <div className="field has-addons" style={{ marginLeft: "0" }}>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            name="search"
+            placeholder="タグを検索"
+          />
+        </div>
+        <div className="control">
+          <a className="button is-success">
+            <i className="fa fa-search"></i>検索
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+function Navigation() {
+  return (
+    <div className="column">
+      <div className={`dropdown is-hoverable`}>
+        <div className="dropdown-trigger">
+          <button
+            className="button is-primary"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+          >
+            <span className="has-text-weight-bold">メニュー</span>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down"></i>
+            </span>
+          </button>
+        </div>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            <a href="#" className="dropdown-item">
+              ホーム
+            </a>
+            <a href="#" className="dropdown-item">
+              使い方
+            </a>
+            <a href="#" className="dropdown-item">
+              このサイトでできること
+            </a>
+            <a href="#" className="dropdown-item">
+              参考文献
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer className="footer has-background-white-ter">
+      <div className="content has-text-centered">
+        <p>2021 QiitaViz</p>
+      </div>
+    </footer>
+  );
 }
 export default App;
--- src/D3DirectedGraph.js
+++ src/D3DirectedGraph.js
@@ -1,127 +1,213 @@
-import * as d3 from 'd3';
-import { useState, useEffect, useRef } from "react";
-
+import * as d3 from "d3";
+import "bulma/css/bulma.css";
+import { useState, useEffect } from "react";
 function D3DirectedGraph() {
-  const svgWidth = 1500;
-  const svgHeight = 800;
+  const MOBILE_BORDER_SIZE = 599;
 
+  const [loading, setLoading] = useState(true);
+  const [nodes, setNodes] = useState([]);
+  const [links, setLinks] = useState([]);
+  // デバイスの横、縦幅を取得
+  const { innerWidth: deviceWidth, innerHeight: deviceHeight } = window;
+  const svgWidth = 800;
+  const svgHeight = deviceWidth <= MOBILE_BORDER_SIZE ? 1500 : 1100;
 
-  let isload = useRef(false);
-
-  const [nodes_p, setNodes] = useState([]);
-  const [links_p, setLinks] = useState([]);
-
+  const nodeClickHandle = (e) => {
+    window.open(
+      e.currentTarget.dataset.url,
+      "sampleWin",
+      "width=800,height=800,scrollbars=no,status=no,toolbar=no,location=no,menubar=no,resizable=yes"
+    );
+  };
 
   useEffect(() => {
-    let nodes_d3, links_d3;
-    async function fetchData() {
-      const response = await fetch("data/dummy_data.json");
-      const data = await response.json();
-      let nodes = Array();
-      let links = Array();
-      Object.keys(data).forEach((k) => {
-        nodes.push({
-          id: data[k].ID,//nodeのindexを標準設定から変更
-          label: data[k].nodeName,
-          r: 50
-        }
-        )
-        if (data[k].childNodes.length > 0) {
-          for (let i = 0; i < data[k].childNodes.length; i++) {
-            links.push({
-              source: data[k].ID,
-              target: data[k].childNodes[i]
-            }
-            )
-          }
-        }
-      })
-      return [nodes.slice(), links.slice()];
-    }
-
-
-    function startSimulation() {
-      const simulation = d3.forceSimulation()
-        .force("collide", d3.forceCollide().radius(function (d) { return d.r; }).iterations(16)) //衝突値の設定 よくわかってない
-        .force("link", d3.forceLink().distance((d) => 500).id((d) => d.id))//stength:linkの強さ（元に戻る力 distance: linkの長さ
-        .force("charge", d3.forceManyBody().strength(-200))  //引き合う力を設定。
-        .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2))  //描画するときの中心を設定
-        .force("x", d3.forceX().x(svgWidth / 2).strength(0.2))  //x方向に戻る力
-        .force("y", d3.forceY().y(svgHeight / 2).strength(0.2)) //y方向に戻る力   
-        ;
-
+    const startSimulation = (nodes, links) => {
+      const linkLen = deviceWidth <= MOBILE_BORDER_SIZE ? 750 : 150;
+      const simulation = d3
+        .forceSimulation()
+        .force(
+          "collide",
+          d3
+            .forceCollide()
+            .radius(function (d) {
+              return d.r;
+            })
+            .iterations(16)
+        ) //衝突値の設定
+        .force(
+          "link",
+          d3
+            .forceLink()
+            .distance((d) => linkLen)
+            .id((d) => d.id)
+        ) //stength:linkの強さ（元に戻る力 distance: linkの長さ
+        .force("charge", d3.forceManyBody().strength(-500)) //引き合う力を設定。
+        .force("center", d3.forceCenter(svgWidth / 2, svgHeight / 2)) //描画するときの中心を設定
+        .force(
+          "x",
+          d3
+            .forceX()
+            .x(svgWidth / 2)
+            .strength(0.1)
+        ); //x方向に戻る力
+      // .force(
+      //   "r",
+      //   d3
+      //     .forceRadial()
+      //     .radius(svgHeight * 0.35)
+      //     .x(svgWidth / 2)
+      //     .y(svgHeight / 2)
+      //     .strength(0.5)
+      // );
+      // .force(
+      //   "y",
+      //   d3
+      //     .forceY()
+      //     .y(svgHeight / 2)
+      //     .strength(0.01)
+      // ); //y方向に戻る力
       simulation
-        // forceSimulationの影響下にnodesを置く
-        .nodes(nodes_d3)
+        // forceSimulationの影響下に  desを置く
+        .nodes(nodes)
         // 時間経過で動かす
         .on("tick", ticked);
       // linkデータをセット
-      simulation.force("link")
-        .links(links_d3);
-      // setInterval(() => {
+      simulation.force("link").links(links);
 
       function ticked() {
-        isload.current = true;
-        setNodes(nodes_d3.slice())
-        setLinks(links_d3.slice())
+        setNodes(nodes.slice());
+        setLinks(links.slice());
       }
+    };
 
-    }
-    async function startLineChart() {
-      [nodes_d3, links_d3] = await fetchData();
-      startSimulation();
-    }
+    const startLineChart = async () => {
+      const [nodes, links] = await (async () => {
+        const response = await fetch("ordered_data.json");
+        const data = await response.json();
+        const nodes = Array();
+        const links = Array();
+        const r = deviceWidth <= MOBILE_BORDER_SIZE ? 70 : 35;
+
+        for (const item of data) {
+          nodes.push({
+            id: item.ID, //nodeのindexを標準設定から変更
+            label: item.nodeName,
+            url: item.url,
+            r,
+          });
+
+          for (const child of item.childNode) {
+            links.push({
+              source: item.ID,
+              target: child,
+            });
+          }
+        }
+        return [nodes, links];
+      })();
+
+      startSimulation(nodes, links);
+      setLoading(false);
+    };
     startLineChart();
     // １度だけuseEffect()を実行する
-  }, [])
+  }, []);
 
-  if (isload.current === false) {
-    return (
-      <div>
-        loading...
-      </div>
-    )
+  const arrowEdgeX = -35;
+  const arrowEdgeY = -5;
+  const arrowHeight = 10;
+  const arrowWidth = 14;
+  const arrowEdgeEnd = -25;
+
+  if (loading) {
+    return <div>loading...</div>;
   }
 
   return (
-    <svg class="graph" width={svgWidth} height={svgHeight} viewBox={`0 0 ${svgWidth} ${svgHeight}`}>
-      <defs>
-        <marker id="arrowhead" viewBox="-35 -5 10 10" refX="13" refY="0" orient="auto" markerWidth="13" markerHeight="13" xoverflow="visible">
-          <path d="M -35,-5 L -25 ,0 L -35,5" fill="#999" style={{ stroke: "none" }}>
-          </path>
-        </marker>
-      </defs>
+    <div className="container">
+      <svg
+        className="graph has-background-white"
+        style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+        width={deviceWidth <= MOBILE_BORDER_SIZE ? "100%" : svgWidth}
+        height={svgHeight}
+        viewBox={`0 0 ${svgWidth} ${svgHeight}`}
+      >
+        <defs>
+          <marker
+            id="arrowhead"
+            // viewBox="-35 -5 10 10"
+            viewBox={`${arrowEdgeX} ${arrowEdgeY} ${arrowWidth} ${arrowHeight}`}
+            refX="13"
+            refY="0"
+            orient="auto"
+            markerWidth="13"
+            markerHeight="13"
+            xoverflow="visible"
+          >
+            <path
+              // d="M -35,-5 L -25 ,0 L -35,5"
+              d={`M ${arrowEdgeX} ${arrowEdgeY} L ${arrowEdgeEnd} 0 L ${arrowEdgeX} ${
+                -1 * arrowEdgeY
+              }`}
+              fill="#999"
+              style={{ stroke: "none" }}
+            ></path>
+          </marker>
+        </defs>
 
-      <g class="links">
-        {
-          links_p.map((link) => {
+        <g className="links">
+          {links.map((link) => {
             return (
-              <line key={link.source.id + "-" + link.target.id} stroke="black" stroke-width="1" class="link" marker-end="url(#arrowhead)" id="edgepath0"
-                x1={link.source.x} y1={link.source.y} x2={link.target.x} y2={link.target.y}></line>
-            )
-          })
-        }
-      </g>
+              <line
+                key={link.source.id + "-" + link.target.id}
+                stroke="black"
+                strokeWidth="1"
+                className="link"
+                markerEnd="url(#arrowhead)"
+                id="edgepath0"
+                x1={link.source.x}
+                y1={link.source.y}
+                x2={link.target.x}
+                y2={link.target.y}
+              ></line>
+            );
+          })}
+        </g>
 
-      <g class="nodes">
-        {
-          nodes_p.map((node) => {
+        <g className="nodes">
+          {nodes.map((node) => {
             return (
-              <circle r={node.r} style={{ fill: "rgb(128, 255, 191)" }}
-                cx={node.x} cy={node.y}></circle>
-            )
-          })
-        }
-      </g>
-      {
-        nodes_p.map((node) => {
+              <circle
+                className="node"
+                key={node.id}
+                r={node.r}
+                style={{ fill: "rgb(128, 255, 191)" }}
+                cx={node.x}
+                cy={node.y}
+                data-url={node.url}
+                onClick={nodeClickHandle}
+              ></circle>
+            );
+          })}
+        </g>
+
+        {nodes.map((node) => {
           return (
-            <text key={node.id} text-anchor="middle" fill="black" font-size="12px"
-              x={node.x} y={node.y}>{node.label}</text>
-          )
-        })
-      }
-    </svg >
-  )
+            <text
+              className="node-label"
+              key={node.id}
+              textAnchor="middle"
+              fill="black"
+              fontSize={deviceWidth <= MOBILE_BORDER_SIZE ? "30px" : "15px"}
+              x={node.x}
+              y={node.y}
+            >
+              {node.label}
+            </text>
+          );
+        })}
+      </svg>
+    </div>
+  );
 }
 export default D3DirectedGraph;
