8cef7f5981c07a84707fd788a35fa2209d769127
unibon4141 <vhcdyufg@gmail.com>
2021-07-09 17:43:01 +0900

    ノードをホバーしたときにおすすめ記事を表示

--- src/D3DirectedGraph.js
+++ src/D3DirectedGraph.js
@@ -2,18 +2,264 @@ import * as d3 from "d3";
 import "bulma/css/bulma.css";
 import { useState, useEffect } from "react";
 function D3DirectedGraph() {
+  // 仮の記事データ
+  const articleData = [
+    {
+      type: "JavaScript",
+      title: "【JavaScript】JavaScriptにおけるクロージャーとは",
+      url: "https://qiita.com/sho_U/items/1d58d5fb7cb11030f24b",
+    },
+    {
+      type: "JavaScript",
+      title: "【JavaScript】javascriptの基本",
+      url: "https://qiita.com/smkhkc/items/5e3729cffab6591f34c6",
+    },
+    {
+      type: "JavaScript",
+      title: "【JavaScript】JavaScriptの 「this」とは 1",
+      url: "https://qiita.com/sho_U/items/edb7c897a2bab0f86345",
+    },
+    {
+      type: "JavaScript",
+      title: "【JavaScript】JavaScriptにおける変数の参照について",
+      url: "https://qiita.com/sho_U/items/3d64d23fe31ed09e9dcb",
+    },
+    {
+      type: "iOS",
+      title: "iOS概論",
+      url: "https://qiita.com/zousaaan3/items/16de2103aa4efb07e6fd",
+    },
+    {
+      type: "iOS",
+      title: "iOS概論",
+      url: "https://qiita.com/zousaaan3/items/16de2103aa4efb07e6fd",
+    },
+    {
+      type: "iOS",
+      title: "iOS概論",
+      url: "https://qiita.com/zousaaan3/items/16de2103aa4efb07e6fd",
+    },
+    {
+      type: "iOS",
+      title: "iOS概論",
+      url: "https://qiita.com/zousaaan3/items/16de2103aa4efb07e6fd",
+    },
+    {
+      type: "Docker",
+      title: "【Docker】Dockerとは",
+      url: "https://qiita.com/y-hira18/items/b259f5a2a870478a5453",
+    },
+    {
+      type: "Docker",
+      title: "【Docker】Dockerとは",
+      url: "https://qiita.com/y-hira18/items/b259f5a2a870478a5453",
+    },
+    {
+      type: "Docker",
+      title: "【Docker】Dockerとは",
+      url: "https://qiita.com/y-hira18/items/b259f5a2a870478a5453",
+    },
+    {
+      type: "Docker",
+      title: "【Docker】Dockerとは",
+      url: "https://qiita.com/y-hira18/items/b259f5a2a870478a5453",
+    },
+    {
+      type: "CSS",
+      title: "CSS　～CSSとは何か～",
+      url: "https://qiita.com/stroke/items/19123fc34db3e3483c01",
+    },
+    {
+      type: "CSS",
+      title: "CSS　～CSSとは何か～",
+      url: "https://qiita.com/stroke/items/19123fc34db3e3483c01",
+    },
+    {
+      type: "CSS",
+      title: "CSS　～CSSとは何か～",
+      url: "https://qiita.com/stroke/items/19123fc34db3e3483c01",
+    },
+    {
+      type: "CSS",
+      title: "CSS　～CSSとは何か～",
+      url: "https://qiita.com/stroke/items/19123fc34db3e3483c01",
+    },
+    {
+      type: "PHP",
+      title: "PHPでデータベースに接続するときのまとめ",
+      url: "https://qiita.com/mpyw/items/b00b72c5c95aac573b71",
+    },
+    {
+      type: "PHP",
+      title: "PHPでデータベースに接続するときのまとめ",
+      url: "https://qiita.com/mpyw/items/b00b72c5c95aac573b71",
+    },
+    {
+      type: "PHP",
+      title: "PHPでデータベースに接続するときのまとめ",
+      url: "https://qiita.com/mpyw/items/b00b72c5c95aac573b71",
+    },
+    {
+      type: "PHP",
+      title: "PHPでデータベースに接続するときのまとめ",
+      url: "https://qiita.com/mpyw/items/b00b72c5c95aac573b71",
+    },
+    {
+      type: "AWS",
+      title: "javascriptからAWS S3へのアップロード",
+      url: "https://qiita.com/taaachezi/items/ed331ba66ed4fc0f838b",
+    },
+    {
+      type: "AWS",
+      title: "javascriptからAWS S3へのアップロード",
+      url: "https://qiita.com/taaachezi/items/ed331ba66ed4fc0f838b",
+    },
+    {
+      type: "AWS",
+      title: "javascriptからAWS S3へのアップロード",
+      url: "https://qiita.com/taaachezi/items/ed331ba66ed4fc0f838b",
+    },
+    {
+      type: "AWS",
+      title: "javascriptからAWS S3へのアップロード",
+      url: "https://qiita.com/taaachezi/items/ed331ba66ed4fc0f838b",
+    },
+    {
+      type: "Vue.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Vue.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Vue.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Vue.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Node.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Node.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Node.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Node.js",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "HTML",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "HTML",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "HTML",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "HTML",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Rails",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Rails",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Rails",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Rails",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "React",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "React",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "React",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "React",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Ruby",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Ruby",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Ruby",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+    {
+      type: "Ruby",
+      title: "サンプルテキスト",
+      url: "サンプルテキスト",
+    },
+  ];
   const MOBILE_BORDER_SIZE = 599;
 
   const [loading, setLoading] = useState(true);
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
+  // おすすめ記事
+  const [displayArticle, setDisplayArticle] = useState([]);
+  const [mousePosition, setMousePosition] = useState([]);
+
   // デバイスの横、縦幅を取得
   const { innerWidth: deviceWidth, innerHeight: deviceHeight } = window;
   // const svgWidth = 800;
   const svgWidth =
     deviceWidth <= MOBILE_BORDER_SIZE ? deviceWidth * 0.9 : deviceWidth * 0.8;
   const svgHeight = deviceWidth <= MOBILE_BORDER_SIZE ? 1500 : 1100;
-  console.log("mobile width" + deviceWidth);
   const nodeClickHandle = (e) => {
     window.open(
       e.currentTarget.dataset.url,
@@ -21,7 +267,17 @@ function D3DirectedGraph() {
       "width=800,height=800,scrollbars=no,status=no,toolbar=no,location=no,menubar=no,resizable=yes"
     );
   };
-
+  function overHandle(e) {
+    const target = e.currentTarget.dataset.name;
+    const data = articleData.filter((item) => {
+      return item.type == target;
+    });
+    setDisplayArticle(data);
+    setMousePosition([e.pageX, e.pageY]);
+  }
+  function outHandle(e) {
+    setDisplayArticle([]);
+  }
   useEffect(() => {
     const startSimulation = (nodes, links) => {
       const linkLen = deviceWidth <= MOBILE_BORDER_SIZE ? 150 : 150;
@@ -192,7 +448,9 @@ function D3DirectedGraph() {
                 cx={node.x}
                 cy={node.y}
                 data-url={node.url}
+                data-name={node.label}
                 onClick={nodeClickHandle}
+                onMouseEnter={overHandle}
               ></circle>
             );
           })}
@@ -214,6 +472,30 @@ function D3DirectedGraph() {
           );
         })}
       </svg>
+      <div
+        className={displayArticle.length > 0 ? "card show popup" : "card popup"}
+        style={{
+          position: "absolute",
+          left: mousePosition[0],
+          top: mousePosition[1],
+        }}
+        onMouseLeave={outHandle}
+      >
+        <div className="card-content">
+          <div className="content">
+            <p>おすすめの記事</p>
+            {displayArticle.map((item, i) => {
+              return (
+                <p key={i}>
+                  <a href={item.url} target="_blank">
+                    {item.title}
+                  </a>
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
